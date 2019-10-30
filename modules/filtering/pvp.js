43c765253b91f4d755de9f9534f90b66e97b63b7
TallyPokeMap <r.g.levasseur@gmail.com>
2019-10-30 16:20:54 -0400

    PvP filters

--- modules/filtering/pvp.js
+++ modules/filtering/pvp.js
@@ -3,26 +3,20 @@ const pvp = require('../base/pvp.js');
 
 module.exports.run = async (MAIN, sighting, area, server, timezone, role_id) => {
 
-  // IF RUNNING UIV AND POKEMON DOESN'T HAVE IV WAIT UNTIL IT IS RESET BY RDM IF/WHEN IT GETS IV CHECKED
-  if(MAIN.config.UIV != 'DISABLED' && !sighting.cp) { return; }
-
-  if(MAIN.config.PVP.Discord_Feeds != 'ENABLED'){ return; }
-
-  // DON'T FILTER IF FEEDS ARE DISABLED
-  if(MAIN.config.POKEMON.Discord_Feeds != 'ENABLED'){ return; }
+  if(!sighting.cp || sighting.cp == 0){ return };
 
   // VARIABLES
   let internal_value = (sighting.individual_defense+sighting.individual_stamina+sighting.individual_attack)/45;
   let time_now = new Date().getTime(); internal_value = Math.floor(internal_value*1000)/10;
 
   // CHECK ALL FILTERS
-  MAIN.PVP_Channels.forEach((pokemon_channel,index) => {
+  MAIN.PVP_Channels.forEach((pvp_channel,index) => {
 
     // DEFINE FILTER VARIABLES
-    let geofences = pokemon_channel[1].geofences.split(',');
-    let channel = MAIN.channels.get(pokemon_channel[0]);
-    let filter = MAIN.Filters.get(pokemon_channel[1].filter);
-    let embed = pokemon_channel[1].embed ? pokemon_channel[1].embed : 'pvp.js';
+    let geofences = pvp_channel[1].geofences.split(',');
+    let channel = MAIN.channels.get(pvp_channel[0]);
+    let filter = MAIN.Filters.get(pvp_channel[1].filter);
+    let embed = pvp_channel[1].embed ? pvp_channel[1].embed : 'pvp.js';
     let role_id = '';
 
     // DETERMINE GENDER
@@ -33,16 +27,16 @@ module.exports.run = async (MAIN, sighting, area, server, timezone, role_id) => 
     }
 
     // CHECK FOR INVALID DATA
-    if(!filter){ return console.error('['+MAIN.config.BOT_NAME+'] ['+MAIN.Bot_Time(null,'stamp')+'] The filter defined for'+pokemon_channel[0]+' does not appear to exist.'); }
-    if(!channel){ return console.error('['+MAIN.config.BOT_NAME+'] ['+MAIN.Bot_Time(null,'stamp')+'] The channel '+pokemon_channel[0]+' does not appear to exist.'); }
-    if(filter.Type != 'pokemon'){ return console.error('['+MAIN.config.BOT_NAME+'] ['+MAIN.Bot_Time(null,'stamp')+'] The filter defined for'+pokemon_channel[0]+' does not appear to be a pokemon filter.'); }
+    if(!filter){ return console.error('['+MAIN.config.BOT_NAME+'] ['+MAIN.Bot_Time(null,'stamp')+'] The filter defined for'+pvp_channel[0]+' does not appear to exist.'); }
+    if(!channel){ return console.error('['+MAIN.config.BOT_NAME+'] ['+MAIN.Bot_Time(null,'stamp')+'] The channel '+pvp_channel[0]+' does not appear to exist.'); }
+    if(filter.Type != 'pvp'){ return console.error('['+MAIN.config.BOT_NAME+'] ['+MAIN.Bot_Time(null,'stamp')+'] The filter defined for'+pvp_channel[0]+' does not appear to be a pvp filter.'); }
 
     // ADD ROLE ID IF IT EXISTS
-    if(pokemon_channel[1].roleid){
-      if(pokemon_channel[1].roleid == 'here' || pokemon_channel[1].roleid == 'everyone'){
-        role_id = '@'+pokemon_channel[1].roleid;
+    if(pvp_channel[1].roleid){
+      if(pvp_channel[1].roleid == 'here' || pvp_channel[1].roleid == 'everyone'){
+        role_id = '@'+pvp_channel[1].roleid;
       } else{
-        role_id = '<@&'+pokemon_channel[1].roleid+'>';
+        role_id = '<@&'+pvp_channel[1].roleid+'>';
       }
     }
 
@@ -50,7 +44,7 @@ module.exports.run = async (MAIN, sighting, area, server, timezone, role_id) => 
     if(geofences.indexOf(server.name) >= 0 || geofences.indexOf(area.main) >= 0 || geofences.indexOf(area.sub) >= 0){
 
       // no need to calculate possible CP if current CP wasn't provided
-      if(!sighting.cp) { return };
+
       if(sighting.cp > filter.max_cp_range) { return; }
       if(filter[MAIN.masterfile.pokemon[sighting.pokemon_id].name] != 'True'){ return; }
       let possible_cps = pvp.CalculatePossibleCPs(MAIN, sighting.pokemon_id, sighting.form, sighting.individual_attack, sighting.individual_defense, sighting.individual_stamina, sighting.pokemon_level, gender, filter.min_cp_range, filter.max_cp_range);
